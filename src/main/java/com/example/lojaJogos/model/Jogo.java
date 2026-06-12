1ad89599edec66dd8cba6377575d7a807b130e23
Rodrigo Ferrett <161031706+Ferrett27@users.noreply.github.com>
2026-06-12 07:25:16 -0300

    Tirar comentário desnecessário

--- src/main/java/com/example/lojaJogos/model/Jogo.java
+++ src/main/java/com/example/lojaJogos/model/Jogo.java
@@ -17,5 +17,5 @@ public class Jogo {
     private String titulo;
     private BigDecimal preco;
     private LocalDate dataLancamento;
-    private Integer editoraId; // N:1 com Editora
+    private Integer editoraId;
 }
